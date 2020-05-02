10de7f26d3cfaa66587b65f3a3258fe26e263f06
Heatwave <stan_yxr@163.com>
2020-05-02 21:53:04 +0800

    206.reverse-linked-list java version

---
+++ java/src/com/heatwave/leetcode/_0206ReverseLinkedList.java
@@ -0,0 +1,60 @@
+package com.heatwave.leetcode;
+
+/**
+ * 206. Reverse Linked List
+ * <p>
+ * Reverse a singly linked list.
+ * <p>
+ * Example:
+ * <p>
+ * Input: 1->2->3->4->5->NULL
+ * Output: 5->4->3->2->1->NULL
+ * Follow up:
+ * <p>
+ * A linked list can be reversed either iteratively or recursively. Could you implement both?
+ */
+
+public class _0206ReverseLinkedList {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    static class Solution {
+        public ListNode reverseList(ListNode head) {
+            if (head == null || head.next == null) {
+                return head;
+            }
+
+            ListNode newHead = reverseList(head.next);
+            head.next.next = head;
+            head.next = null;
+
+            return newHead;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode listNode = new ListNode(1);
+        listNode.next = new ListNode(2);
+        listNode.next.next = new ListNode(3);
+        listNode.next.next.next = new ListNode(4);
+        listNode.next.next.next.next = new ListNode(5);
+
+        Solution solution = new Solution();
+        listNode = solution.reverseList(listNode);
+
+        for (int i = 5; i > 0; i--) {
+            assert listNode.val == i;
+            listNode = listNode.next;
+        }
+
+        listNode = solution.reverseList(null);
+        assert listNode == null;
+    }
+}
