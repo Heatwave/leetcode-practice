a63dd976c6436a33ccc7f6dd4170d122b101d7ac
Heatwave <stan_yxr@163.com>
2022-12-22 00:01:48 +0800

    update 21. Merge Two Sorted Lists

--- java/src/com/heatwave/leetcode/_0021MergeTwoSortedLists.java
+++ java/src/com/heatwave/leetcode/_0021MergeTwoSortedLists.java
@@ -97,6 +97,33 @@ public class _0021MergeTwoSortedLists {
         }
     }
 
+    static class AnotherSolution {
+        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+            ListNode dummy = new ListNode();
+            ListNode p = dummy;
+
+            while (list1 != null && list2 != null) {
+                if (list1.val < list2.val) {
+                    p.next = list1;
+                    list1 = list1.next;
+                } else {
+                    p.next = list2;
+                    list2 = list2.next;
+                }
+                p = p.next;
+            }
+
+            if (list1 != null) {
+                p.next = list1;
+            }
+            if (list2 != null) {
+                p.next = list2;
+            }
+
+            return dummy.next;
+        }
+    }
+
     static class SolutionRecursive {
         public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
             if (list1 == null && list2 == null) {
