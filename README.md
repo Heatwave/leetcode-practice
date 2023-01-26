d80059add16384511affccd095e828bd96ae9f06
Heatwave <stan_yxr@163.com>
2023-01-26 21:04:44 +0800

    144. Binary Tree Preorder Traversal

--- README.md
+++ README.md
@@ -44,6 +44,7 @@
 - [116. Populating Next Right Pointers in Each Node (填充每个节点的下一个右侧节点指针)](java/src/com/heatwave/leetcode/problems/_0116PopulatingNextRightPointersInEachNode.java) - `BFS (广度优先搜索)`, `DFS (深度优先搜索)`
 - [141. Linked List Cycle (环形链表)](java/src/com/heatwave/leetcode/problems/_0141LinkedListCycle.java) - `Linked List (链表)`, `Double Point (双指针)`
 - [142. Linked List Cycle II (环形链表 II)](java/src/com/heatwave/leetcode/problems/_0142LinkedListCycleii.java) - `Linked List (链表)`, `Double Point (双指针)`, `Hash Table (哈希表)`
+- [144. Binary Tree Preorder Traversal (二叉树的前序遍历)](java/src/com/heatwave/leetcode/problems/_0144BinaryTreePreorderTraversal.java) - `Binary Tree (二叉树)`, `Preorder (前序遍历)`, `DFS (深度优先搜索)`
 - [160. Intersection of Two Linked Lists (相交链表)](java/src/com/heatwave/leetcode/problems/_0160IntersectionOfTwoLinkedLists.java) - `Linked List (链表)`, `Hash Table (哈希表)`
 - [167. Two Sum II - Input Array Is Sorted (两数之和 II - 输入有序数组)](java/src/com/heatwave/leetcode/problems/_0167TwoSumiiInputArrayIsSorted.java) - `Array (数组)`, `Double Point (双指针)`
 - [189. Rotate Array (轮转数组)](java/src/com/heatwave/leetcode/problems/_0189RotateArray.java) - `Array (数组)`, `Double Point (双指针)`
---
+++ java/src/com/heatwave/leetcode/problems/_0144BinaryTreePreorderTraversal.java
@@ -0,0 +1,107 @@
+package com.heatwave.leetcode.problems;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+public class _0144BinaryTreePreorderTraversal {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+        List<Integer> ans = new ArrayList<>();
+
+        public List<Integer> preorderTraversal(TreeNode root) {
+            if (root == null) {
+                return ans;
+            }
+
+            ans.add(root.val);
+            preorderTraversal(root.left);
+            preorderTraversal(root.right);
+
+            return ans;
+        }
+    }
+
+    class SolutionAddAll {
+        public List<Integer> preorderTraversal(TreeNode root) {
+            List<Integer> res = new ArrayList<>();
+
+            if (root == null) {
+                return res;
+            }
+
+            res.add(root.val);
+            res.addAll(preorderTraversal(root.left));
+            res.addAll(preorderTraversal(root.right));
+
+            return res;
+        }
+    }
+
+    class SolutionIteration {
+        public List<Integer> preorderTraversal(TreeNode root) {
+            List<Integer> ans = new LinkedList<>();
+            Stack<TreeNode> stack = new Stack<>();
+            while (root != null || !stack.isEmpty()) {
+                while (root != null) {
+                    ans.add(root.val);
+                    stack.add(root);
+                    root = root.left;
+                }
+                root = stack.pop();
+                root = root.right;
+            }
+            return ans;
+        }
+    }
+
+    class SolutionMorris {
+        public List<Integer> preorderTraversal(TreeNode root) {
+            List<Integer> ans = new ArrayList<>();
+            TreeNode predecessor;
+
+            while (root != null) {
+                if (root.left == null) {
+                    ans.add(root.val);
+                    root = root.right;
+                    continue;
+                }
+
+                predecessor = root.left;
+                while (predecessor.right != null && predecessor.right != root) {
+                    predecessor = predecessor.right;
+                }
+                if (predecessor.right == null) {
+                    ans.add(root.val);
+                    predecessor.right = root;
+                    root = root.left;
+                    continue;
+                } else {
+                    predecessor.right = null;
+                }
+                root = root.right;
+            }
+
+            return ans;
+        }
+    }
+}
